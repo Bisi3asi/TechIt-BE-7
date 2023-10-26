69df8df7f2b27d04a69487d4a8d8280661d7c3c0
Bisi3asi <kueeng8008@naver.com>
2023-10-26 20:27:18 +0900

    mod : Rq 예외 처리 1. 사용자의 입력값이 ?가 없는 경우 생성과정에서 init exception 발생  - split에 따른 배열 길이로 처리

--- WiseApp/src/main/java/com/WiseApp/Rq.java
+++ WiseApp/src/main/java/com/WiseApp/Rq.java
@@ -6,36 +6,45 @@ public class Rq {
     String cmd;
     String action;
     String queryString;
-    HashMap<String, String> paramMap;
+    HashMap<String, String> paramMap = new HashMap<>();
 
-    Rq(String cmd) {
+    Rq (String cmd) {
         this.cmd = cmd;
-        String[] cmdBits = cmd.split("\\?, 2");
-        this.action = cmdBits[0].trim();
-        this.queryString = cmdBits[1].trim();
 
+        String[] cmdBits = cmd.split("\\?", 2);
+        if (cmdBits.length == 2) {
+            this.action = cmdBits[0].trim();
+            this.queryString = cmdBits[1].trim();
+        }
+        if (cmdBits.length == 1) {
+            this.action = cmdBits[0].trim();
+            this.queryString ="";
+        }
         String[] queryStringBits = queryString.split("&");
+
         for (int i = 0; i < queryStringBits.length; i++) {
             String queryParamStr = queryStringBits[i];
             String[] queryParamStrBits = queryParamStr.split("=", 2);
 
             String paramName = queryParamStrBits[0];
             String paramValue = queryParamStrBits[1];
-            paramMap.put(paramName, paramValue);
+            if (paramName != null && paramValue != null ) {
+                paramMap.put(paramName, paramValue);
+            }
         }
     }
 
-    String getAction(){
+    String getAction() {
         return action;
     }
 
-    public int getParamAsInt(String paramName, int defaultValue){
+
+    public int getParamValueAsInt(String paramName, int defaultValue) {
         String paramValue = paramMap.get(paramName);
         try {
             return Integer.parseInt(paramValue);
         } catch (NumberFormatException e) {
             return defaultValue;
         }
-
     }
 }
