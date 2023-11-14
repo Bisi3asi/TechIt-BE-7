a93fa0bc61f6d1e7c537da79aa50c34edaf0a0ac
Bisi3asi <kueeng8008@naver.com>
2023-11-14 21:52:11 +0900

    feat : response 값의 json 전송 방식

--- SpringBootDemo/src/main/java/com/example/springbootdemo/HomeController.java
+++ SpringBootDemo/src/main/java/com/example/springbootdemo/HomeController.java
@@ -1,9 +1,15 @@
 package com.example.springbootdemo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -37,14 +43,14 @@ public class HomeController {
         return "a : " + a + " b : " + b;
     }
 
-        // URI로 받는 Request 값은 모두 String 형이다.
-        // 따라서 Request 변수 설정 시 String으로 받아야 다양한 데이터 받을 수 있다.
+    // URI로 받는 Request 값은 모두 String 형이다.
+    // 따라서 Request 변수 설정 시 String으로 받아야 다양한 데이터 받을 수 있다.
     @GetMapping("/calc3")
     @ResponseBody
     String showCalc3(
             @RequestParam(defaultValue = "0") int a,
             @RequestParam(defaultValue = "0") int b) {
-        return "계산결과 : %d".formatted(a+b);
+        return "계산결과 : %d".formatted(a + b);
     }
 
     @GetMapping("/calc4")
@@ -52,7 +58,7 @@ public class HomeController {
     String showCalc4(
             @RequestParam(defaultValue = "0") double a,
             @RequestParam(defaultValue = "0") double b) {
-        return "계산결과 : %f".formatted(a+b);
+        return "계산결과 : %f".formatted(a + b);
     }
 
     @GetMapping("/calc5")
@@ -60,6 +66,71 @@ public class HomeController {
     String showCalc5(
             @RequestParam(defaultValue = "0") String a,
             @RequestParam(defaultValue = "0") String b) {
-        return "계산결과 : %s".formatted(a+b);
+        return "계산결과 : %s".formatted(a + b);
     }
+
+    // 브라우저로 보내는 Response 값이 int 인 경우 :
+    // 함수의 리턴값을 스프링이 문자열로 캐스팅해 브라우저로 전송한다
+    @GetMapping("/calc6")
+    @ResponseBody
+    int showCalc6(@RequestParam(defaultValue = "0") int a, @RequestParam(defaultValue = "0") int b) {
+        return a + b;
+    }
+
+    // java에서의 리턴값은 java에서만 유효하며, 클라이언트로 전송되는 리턴값은 모두 String화
+    @GetMapping("/calc7")
+    @ResponseBody
+    boolean showCalc7(@RequestParam(defaultValue = "0") int a, @RequestParam(defaultValue = "0") int b) {
+        return a > b;
+    }
+
+    // 객체를 리턴할 시는 객체를 String화 해서 json 방식으로 리턴한다
+    @GetMapping("/calc8")
+    @ResponseBody
+    Person showCalc8(String name, int age) {
+        // private 필드에 Getter를 쓰면 가져온다.
+        return new Person(name, age);
+    }
+
+    // keyMap value = object형으로 uri에서 받은 값을 클라이언트에게 전달하기
+    @GetMapping("/calc9")
+    @ResponseBody
+    Map<String, Object> showCalc9(String name, int age) {
+        return Map.of(
+                "name", name,
+                "age", age
+        );
+    }
+
+    // return List
+    @GetMapping("/calc10")
+    @ResponseBody
+    List<Person> showCalc10(String name, int age) {
+        return new ArrayList<>(){{
+            add(new Person(name, age));
+            add(new Person("Tom", 42));
+            add(new Person("Ginger", 12));
+        }};
+    }
+
+    // Arrays
+    @GetMapping("/calc11")
+    @ResponseBody
+    int[] showCalc11() {
+        return new int[]{10, 50, 40};
+    }
+
+    @GetMapping("/calc12")
+    @ResponseBody
+    int[] showCalc12() {
+        return new int[]{10, 50, 40};
+    }
+}
+
+@AllArgsConstructor
+class Person{
+    @Getter
+    private String name;
+    @Getter
+    private int age;
 }
