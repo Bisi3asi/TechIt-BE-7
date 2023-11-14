a35a94ec8789a7991d64eb59599364afa6f55fa4
Bisi3asi <kueeng8008@naver.com>
2023-11-14 21:09:06 +0900

    feat : URI로 값을 request
    1. primitive type : null X
    2. 별도 URI로 값 미입력 시 null 반환

--- SpringBootDemo/src/main/java/com/example/springbootdemo/HomeController.java
+++ SpringBootDemo/src/main/java/com/example/springbootdemo/HomeController.java
@@ -18,4 +18,19 @@ public class HomeController {
     String showAbout(){
         return "개발자 커뮤니티";
     }
+
+    @GetMapping("/calc")
+    @ResponseBody
+    // 매개 값을 주지 않으면 Spring에서는 자동으로 인자에 null 값을 넣으려고 시도한다.
+    String showCalc(int a, int b){ // null 값을 집어 넣을 수 없는 primitive type이므로 error 500 반환
+        return "계산기";
+    }
+
+    @GetMapping("/calc2")
+    @ResponseBody
+    String showCalc2(Integer a, Integer b){ // Integer에는 null 값이 지원된다.
+        // URI로 calc2/?a=100%b=200 주면 값 출력
+        // 안주면 null 출력
+        return "a : " + a + " b : " + b;
+    }
 }
