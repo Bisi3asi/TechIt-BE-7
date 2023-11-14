b6804bbeaaac0035e5d7e7b4099fcc7fdd50a050
Bisi3asi <kueeng8008@naver.com>
2023-11-15 01:39:32 +0900

    refactor : rs code to Class

--- SpringBootDemo/src/main/java/com/example/springbootdemo/ArticleController.java
+++ SpringBootDemo/src/main/java/com/example/springbootdemo/ArticleController.java
@@ -7,40 +7,59 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/article")
 public class ArticleController {
-    private Article lastArticle;
+    private List<Article> articles = new ArrayList<>();
 
     @GetMapping("/write")
-    String showWrite(){
+    String showWrite() {
         return "write";
     }
 
     @GetMapping("/doWrite")
     @ResponseBody
-    Map<String, Object> doWrite(String title, String body){
+    RsData<Article> doWrite(String title, String body) {
+        Article article = new Article(articles.size() + 1, title, body);
+        articles.add(article);
 
-        Map<String, Object> rs = new HashMap<>();
-        lastArticle = new Article(1, title, body);
-        rs.put("msg", "1번 게시물이 작성되었습니다.");
-        rs.put("data", lastArticle);
+        RsData<Article> rs = new RsData(
+                "S-1",
+                "%d번 게시물이 작성되었습니다.".formatted(article.getId()),
+                article
+        );
+
         return rs;
     }
 
     @GetMapping("/getLastArticle")
     @ResponseBody
-    Article getLastArticle(){
-        return lastArticle;
+    Article getLastArticle() {
+        // getLast는 java 21 버전에서만 지원
+        return articles.getLast();
+    }
+
+    @GetMapping("/getArticles")
+    @ResponseBody
+    List<Article> getArticles() {
+        return articles;
     }
 }
 
 @Getter
 @AllArgsConstructor
-class Article{
+class RsData<T> {
+    private String resultCode;
+    private String message;
+    private T data;
+}
+
+@Getter
+@AllArgsConstructor
+class Article {
     private long id;
     private String title;
     private String body;
