ca362ef7d9e7640629c56d08404407709ced5a39
Bisi3asi <kueeng8008@naver.com>
2023-11-15 13:54:05 +0900

    feat : 생성자 주입

--- SpringBootDemo/src/main/java/com/example/springbootdemo/domain/article/article/controller/ArticleController.java
+++ SpringBootDemo/src/main/java/com/example/springbootdemo/domain/article/article/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.example.springbootdemo.domain.article.article.controller;
 import com.example.springbootdemo.domain.article.article.entity.Article;
 import com.example.springbootdemo.domain.article.article.service.ArticleService;
 import com.example.springbootdemo.global.rsData.RsData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +15,12 @@ import java.util.List;
 @Controller
 @RequestMapping("/article")
 public class ArticleController {
-    private final ArticleService articleService = new ArticleService();
+    private final ArticleService articleService;
+
+    @Autowired // 필드에 생성자가 하나 밖에 없으면 생략 가능하다
+    public ArticleController(ArticleService articleService){
+        this.articleService = articleService;
+    }
 
     @GetMapping("/write")
     String showWrite() {
--- SpringBootDemo/src/main/java/com/example/springbootdemo/domain/article/article/service/ArticleService.java
+++ SpringBootDemo/src/main/java/com/example/springbootdemo/domain/article/article/service/ArticleService.java
@@ -10,8 +10,12 @@ import java.util.List;
 @Service // 저는 단 한번만 생성되고, 그 이후에는 재사용되는 객체입니다.
 // Component와 동일
 public class ArticleService {
-    @Autowired
-    private final ArticleRepository articleRepository = new ArticleRepository();
+    private final ArticleRepository articleRepository;
+
+    @Autowired // 필드에 생성자가 하나 밖에 없으면 생략 가능하다
+    public ArticleService(ArticleRepository articleRepository){
+        this.articleRepository = articleRepository;
+    }
 
     public Article write(String title, String body){
         Article article = new Article(title, body);
