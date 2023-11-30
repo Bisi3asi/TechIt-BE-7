11389e18d8a0352ede46dcb86d8987eedc152c8a
Bisi3asi <kueeng8008@naver.com>
2023-11-30 20:43:05 +0900

    test : 테스트 환경 구동

--- RestSbb/src/test/java/com/mysite/restsbb/RestSbbApplicationTests.java
+++ RestSbb/src/test/java/com/mysite/restsbb/RestSbbApplicationTests.java
@@ -2,8 +2,10 @@ package com.mysite.restsbb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // 테스트 환경에서만 작동한다.
 class RestSbbApplicationTests {
 
 	@Test
