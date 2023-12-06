70696e22dcbfdbb0293cdd6136b27adfb9d26f42
Bisi3asi <kueeng8008@naver.com>
2023-12-07 01:44:20 +0900

    feat: filter에 가짜 유저를 넣어서 로그인 된 것처럼 처리

--- RestSbb/src/main/java/com/mysite/restsbb/global/security/JwtAuthenticationFilter.java
+++ RestSbb/src/main/java/com/mysite/restsbb/global/security/JwtAuthenticationFilter.java
@@ -5,8 +5,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -14,6 +20,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     @SneakyThrows
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
+
+        User user = new User("user1", "", List.of());
+
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+                // filter에 가짜 유저를 넣어서 로그인 된 것처럼 처리
+                user,
+                user.getPassword(),
+                user.getAuthorities()
+        );
+
+        // Security 상에서 getContext.setAuthentication으로 Principal 사용 가능
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
         filterChain.doFilter(request, response);
     }
 }
