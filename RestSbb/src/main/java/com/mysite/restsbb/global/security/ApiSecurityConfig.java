cebb1b74c66840599d8a764f6b9d0965db974b50
Bisi3asi <kueeng8008@naver.com>
2023-12-07 01:33:27 +0900

    feat: jwtAuthenticationFilter 도입

--- RestSbb/src/main/java/com/mysite/restsbb/global/security/ApiSecurityConfig.java
+++ RestSbb/src/main/java/com/mysite/restsbb/global/security/ApiSecurityConfig.java
@@ -5,13 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class ApiSecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
     @Bean
     // Get은 발생하지 않는 csrf 토큰 문제에 대해 Delete, Post는 발생
     // /api/로 시작하는 url에 대해 csrf를 disable
@@ -20,6 +22,7 @@ public class ApiSecurityConfig {
                 // securityMatcher(url) 이후로 나오는 모든 작업은 해당 패턴에 해당하는 요청에만 적용
                 .securityMatcher("/api/**")
                 .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
         ;
         return http.build();
     }
