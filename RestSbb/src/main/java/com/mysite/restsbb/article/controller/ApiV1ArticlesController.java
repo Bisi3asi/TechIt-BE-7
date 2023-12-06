9855823acd29472ac295cd9c8d74de9e1dee7164
Bisi3asi <kueeng8008@naver.com>
2023-12-07 01:12:20 +0900

    feat : PUT / modifyArticle 추가

--- RestSbb/src/main/java/com/mysite/restsbb/article/controller/ApiV1ArticlesController.java
+++ RestSbb/src/main/java/com/mysite/restsbb/article/controller/ApiV1ArticlesController.java
@@ -6,6 +6,7 @@ import com.mysite.restsbb.article.entity.Article;
 import com.mysite.restsbb.global.rsdata.RsData;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,53 +18,96 @@ import java.util.Map;
 public class ApiV1ArticlesController {
     private final ArticleService articleService;
 
+    // GET (Articles)
     @Getter
     public static class GetArticlesResponseBody {
         private final List<ArticleDto> items;
         private final Map pagination;
 
-        public GetArticlesResponseBody(List<Article> articles){
+        public GetArticlesResponseBody(List<Article> articles) {
             items = articles.stream()
                     .map(ArticleDto::new)
                     .toList();
             pagination = Map.of("page", 1);
         }
     }
+
     @GetMapping("")
-    public RsData<GetArticlesResponseBody> getArticles(){
+    public RsData<GetArticlesResponseBody> getArticles() {
         return RsData.of("200", "success",
                 new GetArticlesResponseBody(articleService.findAllByOrderByIdDesc()));
     }
 
+    // GET (Article)
     @Getter
     public static class GetArticleResponseBody {
         private final ArticleDto item;
-        public GetArticleResponseBody(Article article){
+
+        public GetArticleResponseBody(Article article) {
             item = new ArticleDto(article);
         }
     }
 
     @GetMapping("/{id}")
-    public RsData<GetArticleResponseBody> getArticle(@PathVariable long id){
+    public RsData<GetArticleResponseBody> getArticle(@PathVariable long id) {
         return RsData.of("200", "success",
                 new GetArticleResponseBody(articleService.findById(id).get()));
     }
 
+    // DELETE (Article)
     @Getter
     public static class RemoveArticleResponseBody {
         private final ArticleDto item;
 
-        public RemoveArticleResponseBody(Article article){
+        public RemoveArticleResponseBody(Article article) {
             item = new ArticleDto(article);
         }
     }
 
     @DeleteMapping("/{id}")
-    public RsData<RemoveArticleResponseBody> removeArticle(@PathVariable long id){
+    public RsData<RemoveArticleResponseBody> removeArticle(@PathVariable long id) {
         Article article = articleService.findById(id).get();
         articleService.deleteById(id);
 
         return RsData.of("200", "success",
                 new RemoveArticleResponseBody(article));
     }
+
+    // PUT (Article)
+    @Getter
+    @Setter
+    public static class ModifyArticleRequestBody {
+        private String title;
+        private String body;
+    }
+
+    @Getter
+    public static class ModifyArticleResponseBody {
+        private final ArticleDto item;
+
+        public ModifyArticleResponseBody(Article article) {
+            item = new ArticleDto(article);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public RsData<ModifyArticleResponseBody> modifyArticle(
+            @PathVariable long id,
+            @RequestBody ModifyArticleRequestBody body
+    ) {
+        Article article = articleService.findById(id).get();
+
+        articleService.modify(article, body.getTitle(), body.getBody());
+
+        return RsData.of(
+                "200",
+                "성공",
+                new ModifyArticleResponseBody(
+                        article
+                )
+        );
+    }
+
+    // post
+
 }
