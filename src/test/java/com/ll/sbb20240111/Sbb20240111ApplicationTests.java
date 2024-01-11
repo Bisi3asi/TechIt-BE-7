58880927e77e8849b4a16d48b666931440ae1074
jhs512 <jangka512@gmail.com>
2024-01-11 13:19:35 +0900

    work

---
+++ src/test/java/com/ll/sbb20240111/Sbb20240111ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sbb20240111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb20240111ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
