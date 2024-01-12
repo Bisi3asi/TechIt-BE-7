d9a1c0e3e3ac065fd24c83c865e518a04168e11e
jhs512 <jangka512@gmail.com>
2024-01-12 09:44:42 +0900

    work

---
+++ src/test/java/com/ll/sbb20240111/batch/BatchTestConfig.java
@@ -0,0 +1,23 @@
+package com.ll.sbb20240111.batch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BatchTestConfig {
+    @Bean
+    public JobLauncherTestUtils helloJobLauncherTestUtils(Job helloJob) {
+        JobLauncherTestUtils utils = new JobLauncherTestUtils();
+        utils.setJob(helloJob);
+        return utils;
+    }
+
+    @Bean
+    public JobLauncherTestUtils hello2JobLauncherTestUtils(Job hello2Job) {
+        JobLauncherTestUtils utils = new JobLauncherTestUtils();
+        utils.setJob(hello2Job);
+        return utils;
+    }
+}
