a5e3d0f5e7cbdf510dc143b35244e42afcbfbc44
jhs512 <jangka512@gmail.com>
2024-01-15 11:24:03 +0900

    work

--- src/main/java/com/ll/sbb20240111/batch/Hello3JobConfig.java
+++ src/main/java/com/ll/sbb20240111/batch/Hello3JobConfig.java
@@ -5,7 +5,11 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.builder.FlowJobBuilder;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.job.flow.support.SimpleFlow;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
@@ -13,6 +17,7 @@ import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Slf4j
@@ -25,15 +30,28 @@ public class Hello3JobConfig {
             Step hello3Step2,
             Step hello3Step3
     ) {
-        return new JobBuilder("hello3Job", jobRepository)
+        Flow flow1 = new FlowBuilder<SimpleFlow>("flow1")
                 .start(hello3Step1)
-                .next(hello3Step2)
-                .next(hello3Step3)
-                .incrementer(new RunIdIncrementer())
                 .build();
+
+        Flow flow2 = new FlowBuilder<SimpleFlow>("flow2")
+                .start(hello3Step2)
+                .build();
+
+        Flow parallelFlow = new FlowBuilder<SimpleFlow>("parallelFlow")
+                .split(new SimpleAsyncTaskExecutor()) // 병렬 실행을 위한 실행기 설정
+                .add(flow1, flow2)
+                .build();
+
+        FlowJobBuilder jobBuilder = new JobBuilder("hello3Job", jobRepository)
+                .start(parallelFlow)
+                .next(hello3Step3)
+                .end()
+                .incrementer(new RunIdIncrementer());
+
+        return jobBuilder.build();
     }
 
-    @JobScope
     @Bean
     public Step hello3Step1(JobRepository jobRepository, Tasklet hello3Step1Tasklet, PlatformTransactionManager platformTransactionManager) {
         return new StepBuilder("hello3Step1Tasklet", jobRepository)
@@ -50,7 +68,6 @@ public class Hello3JobConfig {
         });
     }
 
-    @JobScope
     @Bean
     public Step hello3Step2(JobRepository jobRepository, Tasklet hello3Step2Tasklet, PlatformTransactionManager platformTransactionManager) {
         return new StepBuilder("hello3Step2Tasklet", jobRepository)
