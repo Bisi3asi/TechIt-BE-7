0ddfdb932ff52971394f9d1907acc57db4591c21
Bisi3asi <kueeng8008@naver.com>
2024-01-19 16:39:37 +0900

    feat: entity id auto generation 추가

--- sbrest/src/main/java/com/example/sbrest/domain/article/entity/Article.java
+++ sbrest/src/main/java/com/example/sbrest/domain/article/entity/Article.java
@@ -5,6 +5,8 @@ import com.example.sbrest.domain.users.entity.Users;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -23,6 +25,7 @@ import lombok.ToString;
 @ToString
 public class Article {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(nullable = false, length = 50)
--- sbrest/src/main/java/com/example/sbrest/domain/users/entity/Users.java
+++ sbrest/src/main/java/com/example/sbrest/domain/users/entity/Users.java
@@ -11,6 +11,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,6 +29,7 @@ import lombok.ToString;
 @ToString(callSuper = true)
 public class Users {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(nullable = false, length = 20, unique = true)
