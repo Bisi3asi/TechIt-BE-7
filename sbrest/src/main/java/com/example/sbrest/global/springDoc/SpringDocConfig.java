12c000e660f33db763bce6ab01ca65e0173df7f4
Bisi3asi <kueeng8008@naver.com>
2024-01-22 02:15:27 +0900

    feat: swagger-ui 적용
    * todo: Actuator 오류 문제 해결

---
+++ sbrest/src/main/java/com/example/sbrest/global/springDoc/SpringDocConfig.java
@@ -0,0 +1,11 @@
+package com.example.sbrest.global.springDoc;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "RESTSB_API", version = "v1"))
+public class SpringDocConfig {
+}
