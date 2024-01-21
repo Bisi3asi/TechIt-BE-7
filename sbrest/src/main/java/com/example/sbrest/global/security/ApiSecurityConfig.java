0c1b06de6954b91eaedfa7231ec543b87db74cff
Bisi3asi <kueeng8008@naver.com>
2024-01-22 02:06:01 +0900

    fix: filterchain 수정

--- sbrest/src/main/java/com/example/sbrest/global/security/ApiSecurityConfig.java
+++ sbrest/src/main/java/com/example/sbrest/global/security/ApiSecurityConfig.java
@@ -1,18 +1,15 @@
 package com.example.sbrest.global.security;
 
-import static org.springframework.security.config.http.SessionCreationPolicy.*;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 import com.example.sbrest.global.jwt.JwtAuthenticationFilter;
 
@@ -27,9 +24,14 @@ public class ApiSecurityConfig {
 
 	@Bean
 	@SneakyThrows
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain filterChain(HttpSecurity http) {
 		http.
-			securityMatcher("/api/**")
+			authorizeRequests(
+				authorizeRequests -> authorizeRequests
+					.requestMatchers("/h2-console/**").permitAll()
+					.requestMatchers("/actuator/**").permitAll()
+			)
+			.securityMatcher("/api/**")
 			.authorizeRequests(
 				authorizeRequests -> authorizeRequests
 					.requestMatchers(HttpMethod.GET, "api/*/members/logout").authenticated()
@@ -37,16 +39,8 @@ public class ApiSecurityConfig {
 					.anyRequest().permitAll()
 			)
 			.csrf(AbstractHttpConfigurer::disable) // CSRF 보호 비활성화
-			.sessionManagement(s ->
-				s.sessionCreationPolicy(STATELESS)
-					.sessionFixation(SessionManagementConfigurer.SessionFixationConfigurer::none)) // 세션 생성 비활성화
-			.headers(
-				headers ->
-					headers.frameOptions(
-						frameOptions ->
-							frameOptions.sameOrigin()
-					)
-			)
+			.sessionManagement(AbstractHttpConfigurer::disable) // session 아예 꺼버림
+			.cors(cors -> cors.configure(http)) // cors 설정 활성화
 			.formLogin(AbstractHttpConfigurer::disable) // 기본 로그인 비활성화
 			.logout(AbstractHttpConfigurer::disable) // 기본 로그아웃 비활성화
 			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
