99e7eb14a211e17d65de83e951d3e30bb5f72d81
Bisi3asi <kueeng8008@naver.com>
2024-01-22 17:25:44 +0900

    feat: axios 도입

--- sbrest/frontapp/src/App.js
+++ sbrest/frontapp/src/App.js
@@ -1,21 +1,64 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
 function App() {
-  const[articleList, setArticleList] = useState(null);
+  const [articleList, setArticleList] = useState(null);
 
-
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:8090/api/v1/articles");
+    setArticleList(response.data);
+  };
   useEffect(() => {
-  fetch('http://localhost:8090/api/v1/articles',{
-    method: 'GET',
-    mode: 'cors',
-  })
-  .then((response) => response.json())
-  .then((data) => setArticleList(data));
+    fetchData();
   }, []);
+
+  const onLoginHandler = async (e) => {
+    e.preventDefault();
+    const username = e.target.username.value;
+    const password = e.target.password.value;
+
+    await axios.post("http://localhost:8090/api/v1/users/login", 
+    {username, password}, {withCredentials: true})
+    fetchData();
+  };
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    const title = e.target.title.value;
+    const content = e.target.content.value;
+    await axios.post("http://localhost:8090/api/v1/articles", {title, content}, {withCredentials: true})
+    fetchData();
+  };
 
   return (
     <div className="App">
-      <h1>REST SB 게시판</h1>
+      <form onSubmit={onLoginHandler}>
+        <p>
+          아이디 : <input name="username" />
+        </p>
+        <p>
+          비밀번호: <input name="password" type="password" />
+        </p>
+        <p>
+          <input type="submit" value="로그인하기" />
+        </p>
+        <h1>REST SB 게시판</h1>
+      </form>
+      <form onSubmit={onSubmitHandler}>
+        <p>글 제목</p>
+        <p>
+          <input name="title" />
+        </p>
+        <p>글 내용</p>
+        <p>
+          <input name="content" />
+        </p>
+        <p>
+          <input type="submit" value="등록하기" />
+        </p>
+      </form>
+      <p></p>
       {articleList && (
         <table>
           <thead>
