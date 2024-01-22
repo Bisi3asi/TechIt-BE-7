b8189a852dfb3f5c0723803f05ca8ca6fc4a43da
Bisi3asi <kueeng8008@naver.com>
2024-01-22 10:40:19 +0900

    feat: Spring Doc Grouping

--- sbrest/src/main/java/com/example/sbrest/global/springDoc/SpringDocConfig.java
+++ sbrest/src/main/java/com/example/sbrest/global/springDoc/SpringDocConfig.java
@@ -1,5 +1,7 @@
 package com.example.sbrest.global.springDoc;
 
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
@@ -8,4 +10,12 @@ import io.swagger.v3.oas.annotations.info.Info;
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "RESTSB_API", version = "v1"))
 public class SpringDocConfig {
+
+	@Bean
+	public GroupedOpenApi apiGroupV1(){
+		return GroupedOpenApi.builder()
+			.group("APIv1")
+			.pathsToMatch("/api/v1/**")
+			.build();
+	}
 }
