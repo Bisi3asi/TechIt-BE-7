f8d10580f22ac6ddf1805209a4bb07bd7f682852
Bisi3asi <kueeng8008@naver.com>
2024-02-21 14:39:29 +0900

    learn: 각자의 Job 테스트 환경 구성

---
+++ practice-sbb/src/test/java/com/example/practicesbb/batch/BatchTest.java
@@ -0,0 +1,32 @@
+package com.example.practicesbb.batch;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@SpringBatchTest
+@ActiveProfiles("test")
+public class BatchTest {
+	@Autowired
+	private JobLauncherTestUtils helloJobLauncherTestUtils;
+	@Autowired
+    private JobLauncherTestUtils hello2JobLauncherTestUtils;
+
+	@DisplayName("t1")
+	@Test
+	public void t1() throws Exception {
+        helloJobLauncherTestUtils.launchJob();
+    }
+
+	@DisplayName("t2")
+	@Test
+	public void t2() throws Exception {
+		hello2JobLauncherTestUtils.launchJob();
+	}
+
+}
---
+++ practice-sbb/src/test/java/com/example/practicesbb/batch/BatchTestConfig.java
@@ -0,0 +1,24 @@
+package com.example.practicesbb.batch;
+
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.batch.core.Job;
+
+@Configuration
+public class BatchTestConfig {
+
+	@Bean
+	public JobLauncherTestUtils helloJobLauncherTestUtils(Job helloJob) {
+		JobLauncherTestUtils utils = new JobLauncherTestUtils();
+		utils.setJob(helloJob);
+		return utils;
+	}
+
+	@Bean
+	public JobLauncherTestUtils hello2JobLauncherTestUtils(Job hello2Job) {
+		JobLauncherTestUtils utils = new JobLauncherTestUtils();
+		utils.setJob(hello2Job);
+		return utils;
+	}
+}
